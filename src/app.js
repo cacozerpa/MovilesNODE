2490ead9a68bec3757bd17a2e85846cc73b34bb3
Unknown <zerpa2007@hotmail.com>
2021-06-02 00:28:49 -0400

    authproduct created

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const AuthRoutes = require('./routes/auth');
 
 const app = express();
 
+app.use(express.static('./public'));
+
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,9 +1,16 @@
 const querys = {
+  //User
   GET_USERS: `SELECT * FROM public."Users"`,
   CREATE_USER: `INSERT INTO public."Users" (name, username, password) VALUES ($1, $2, $3) RETURNING *`,
   GET_USERBYID: `SELECT * FROM public."Users" WHERE id = $1`,
   UPDATE_USER: `UPDATE public."Users" SET name = $1, username = $2 WHERE id= $3 RETURNING *`,
-  DELETE_USER: `DELETE FROM public."Users" WHERE id = $1`
+  DELETE_USER: `DELETE FROM public."Users" WHERE id = $1`,
+
+  //Product
+  GET_PRODUCTS: `SELECT * FROM public."Products"`,
+  CREATE_PRODUCT: `INSERT INTO public."Products" (name, rate) VALUES ($1, $2) RETURNING *`,
+  GET_PRODUCTBYID:`SELECT * FROM public."Products" WHERE id = $1`,
+
 };
 
 module.exports = querys;
